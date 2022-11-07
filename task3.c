dfcd9ed5d7304224cbe791faff6964740c0865d3
efreet828 <48798655+efreet828@users.noreply.github.com>
2022-11-07 14:52:12 +0300

    Create task3.c

---
+++ task3.c
@@ -0,0 +1,5 @@
+// план выполнения работы
+/*
+взять за основу https://github.com/seifzadeh/c-network-programming-best-snipts/blob/master/Programming%20raw%20udp%20sockets%20in%20C%20on%20Linux
+ разобрать по известной структуре пакет udp, взять нужные данные, преобразовать в  строку, обратить, вернуть на место. собрать UDP, отправить сырым на порт.
+ что важно, нельзя забывать, что данная программа будет выполняться только под SUDO  структура  и вся инфа по UDP https://tuprints.ulb.tu-darmstadt.de/6243/1/TR-18.pdf*/
